f4eaf07dc23205ea9fc355444d149282f2e11e8d
John Florkey <john.florkey@gmail.com>
2021-04-12 23:37:43 -0400

    working through part 6

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def register():
         db.session.commit()
 
         session['username'] = new_user.username
-        return redirect('/secret')
+        return redirect(f'/users/{new_user.username}')
     else:
         return render_template('user_form.html', form=form)
 
@@ -54,11 +54,29 @@ def login():
         )
         if user:
             session['username'] = user.username
-            return redirect('/secret')
+            return redirect(f'/users/{user.username}')
         else:
             return render_template('login_form.html', form=form)
     else:
         return render_template('login_form.html', form=form)
+
+
+@app.route('/users/<username>', methods=['GET'])
+def user_detail(username):
+    if 'username' not in session or username != session['username']:
+        flash("You must be logged in to view!")
+
+        return redirect('/login')
+    else:
+        user = User.query.filter_by(username=username).first()
+        user_no_pwd = {
+            'username': user.username,
+            'email': user.email,
+            'first_name': user.first_name,
+            'last_name': user.last_name
+        }
+
+        return render_template('user_detail_form.html', user=user_no_pwd)
 
 
 @app.route('/secret', methods=['GET'])
@@ -72,5 +90,6 @@ def secret():
 
 @app.route('/logout', methods=['GET'])
 def logout():
-    session.pop('username')
+    if 'username' in session:
+        session.pop('username')
     return redirect('/')
